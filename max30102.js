ad545dbc5f253a78280800ac4432e3596ec50eb9
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-01 17:32:33 -0600

    Update max30102.js

--- max30102.js
+++ max30102.js
@@ -33,18 +33,20 @@ REG_PART_ID: 0xFF,
 function MAX30102(i2c, options) {
     this.options = options||{};
     this.i2c = i2c;
-    this.ad = C.VL6180X_DEFAULT_I2C_ADDR;
+    this.ad = C.I2C_WRITE_ADDR;
 }
 
 
 MAX30102.prototype.read8 = function(reg) {
+    this.ad = C.I2C_READ_ADDR;
     this.i2c.writeTo(this.ad, reg >> 8, reg & 0xff);
     var data = this.i2c.readFrom(this.ad, 0x01);
     return data[0];
 };
 
 MAX30102.prototype.write8 = function(reg, value) {
-    this.i2c.writeTo(this.ad, reg >> 8, reg & 0xff, value);
+    this.ad = C.I2C_WRITE_ADDR;
+    this.i2c.writeTo(this.ad, reg >> 8, value);
 };
 
 
@@ -54,31 +56,30 @@ MAX30102.prototype.reset = function(){
 
 
 MAX30102.prototype.init = function(){
-  i2c_init();
-  if(!maxim_max30102_write_reg(REG_INTR_ENABLE_1,0xc0)) // INTR setting
-    return false;
-  if(!maxim_max30102_write_reg(REG_INTR_ENABLE_2,0x00))
-    return false;
-  if(!maxim_max30102_write_reg(REG_FIFO_WR_PTR,0x00))  //FIFO_WR_PTR[4:0]
-    return false;
-  if(!maxim_max30102_write_reg(REG_OVF_COUNTER,0x00))  //OVF_COUNTER[4:0]
-    return false;
-  if(!maxim_max30102_write_reg(REG_FIFO_RD_PTR,0x00))  //FIFO_RD_PTR[4:0]
-    return false;
-  if(!maxim_max30102_write_reg(REG_FIFO_CONFIG,0x4f))  //sample avg = 4, fifo rollover=false, fifo almost full = 17
-    return false;
-  if(!maxim_max30102_write_reg(REG_MODE_CONFIG,0x03))   //0x02 for Red only, 0x03 for SpO2 mode 0x07 multimode LED
-    return false;
-  if(!maxim_max30102_write_reg(REG_SPO2_CONFIG,0x27))  // SPO2_ADC range = 4096nA, SPO2 sample rate (100 Hz), LED pulseWidth (411uS)
-    return false;
   
-  if(!maxim_max30102_write_reg(REG_LED1_PA,0x24))   //Choose value for ~ 7mA for LED1
-    return false;
-  if(!maxim_max30102_write_reg(REG_LED2_PA,0x24))   // Choose value for ~ 7mA for LED2
-    return false;
-  if(!maxim_max30102_write_reg(REG_PILOT_PA,0x7f))   // Choose value for ~ 25mA for Pilot LED
-    return false;
-  return true;  
+  this.write8(REG_INTR_ENABLE_1,0xc0); // INTR setting
+    
+  this.write8(REG_INTR_ENABLE_2,0x00);
+    
+  this.write8(REG_FIFO_WR_PTR,0x00);  //FIFO_WR_PTR[4:0]
+    
+  this.write8(REG_OVF_COUNTER,0x00);  //OVF_COUNTER[4:0]
+    
+  this.write8(REG_FIFO_RD_PTR,0x00);  //FIFO_RD_PTR[4:0]
+    
+  this.write8(REG_FIFO_CONFIG,0x4f);  //sample avg = 4, fifo rollover=false, fifo almost full = 17
+    
+  this.write8(REG_MODE_CONFIG,0x03);  //0x02 for Red only, 0x03 for SpO2 mode 0x07 multimode LED
+    
+  this.write8(REG_SPO2_CONFIG,0x27);  // SPO2_ADC range = 4096nA, SPO2 sample rate (100 Hz), LED pulseWidth (411uS)
+    
+  
+  this.write8(REG_LED1_PA,0x24);  //Choose value for ~ 7mA for LED1
+    
+  this.write8(REG_LED2_PA,0x24);  // Choose value for ~ 7mA for LED2
+    
+  this.write8(REG_PILOT_PA,0x7f);   // Choose value for ~ 25mA for Pilot LED
+     
 };
 
 
