f4dc4b0f440a01259dfce688012e3d8ad420b2e3
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-02 12:37:38 -0600

    Update max30102.js

--- max30102.js
+++ max30102.js
@@ -30,60 +30,59 @@ REG_PART_ID: 0xFF,
 };
 
 
-function MAX30102(i2c, options) {
-    this.options = options||{};
+function MAX30102(i2c) {
     this.i2c = i2c;
-    this.ad = C.I2C_WRITE_ADDR;
+    this.ad = NULL;
 }
 
 
 MAX30102.prototype.read8 = function(reg) {
     this.ad = C.I2C_READ_ADDR;
-    this.i2c.writeTo(this.ad, reg);
-    var data = this.i2c.readFrom(this.ad, 0x01);
+    this.i2c.write8(this.ad, reg);
+    var data = this.i2c.readFrom(this.ad,1);
     return data[0];
 };
 
 MAX30102.prototype.write8 = function(reg, value) {
     this.ad = C.I2C_WRITE_ADDR;
-    this.i2c.writeTo(this.ad, reg, value);
+    this.i2c.writeTo(this.ad, reg);
+    this.i2c.writeTo(this.ad, value);
 };
 
 
 MAX30102.prototype.reset = function(){
-    this.i2c.writeTo(this.ad,C.REG_MODE_CONFIG,0x40);
+    this.i2c.write8(C.REG_MODE_CONFIG,0x40);
 };
 
 
 MAX30102.prototype.init = function(){
   
-  this.write8(C.REG_INTR_ENABLE_1,0xc0); // INTR setting
-    
-  this.write8(C.REG_INTR_ENABLE_2,0x00);
-    
+  this.write8(C.REG_INTR_ENABLE_1, 0xc0); // INTR setting
+  
+  this.write8(C.REG_INTR_ENABLE_2, 0x00);
+   
   this.write8(C.REG_FIFO_WR_PTR,0x00);  //FIFO_WR_PTR[4:0]
+   
+  this.write8(C.REG_OVF_COUNTER, 0x00);  //OVF_COUNTER[4:0]
     
-  this.write8(C.REG_OVF_COUNTER,0x00);  //OVF_COUNTER[4:0]
-    
-  this.write8(C.REG_FIFO_RD_PTR,0x00);  //FIFO_RD_PTR[4:0]
-    
-  this.write8(C.REG_FIFO_CONFIG,0x4f);  //sample avg = 4, fifo rollover=false, fifo almost full = 17
-    
+  this.write8(C.REG_FIFO_RD_PTR, 0x00);  //FIFO_RD_PTR[4:0]
+  
+  this.write8(C.REG_FIFO_CONFIG, 0x0f);  //sample avg = 4, fifo rollover=false, fifo almost full = 17
+  
   this.write8(C.REG_MODE_CONFIG,0x03);  //0x02 for Red only, 0x03 for SpO2 mode 0x07 multimode LED
-    
+  
   this.write8(C.REG_SPO2_CONFIG,0x27);  // SPO2_ADC range = 4096nA, SPO2 sample rate (100 Hz), LED pulseWidth (411uS)
     
-  
   this.write8(C.REG_LED1_PA,0x24);  //Choose value for ~ 7mA for LED1
-    
+
   this.write8(C.REG_LED2_PA,0x24);  // Choose value for ~ 7mA for LED2
-    
+
   this.write8(C.REG_PILOT_PA,0x7f);   // Choose value for ~ 25mA for Pilot LED
      
 };
 
 
 
-exports.connect = function(i2c, options) {
-  return new MAX30102(i2c, options);
+exports.connect = function(i2c) {
+  return new MAX30102(i2c);
 };
