5b1372af7a6154dd8016e7b330ca5723c8445c90
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-04 01:58:11 -0600

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -37,7 +37,7 @@ function MAX30102(i2c) {
 
 MAX30102.prototype.read8 = function(reg) {
     this.write8(this.ad, reg);
-    return this.i2c.readFrom(this.ad,1);
+    return this.i2c.readFrom(this.ad,8);
 };
 
 MAX30102.prototype.write8 = function(reg, value) {
