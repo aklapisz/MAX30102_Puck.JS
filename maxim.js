31781b364fce0d71a833e4c87238e19c86b09ad8
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-05 00:36:13 -0600

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -39,9 +39,11 @@ MAX30102.prototype.read8 = function(reg) {
     return this.i2c.readFrom(this.ad,1);
 };
 
+
 MAX30102.prototype.write8 = function(reg, value) {
     this.i2c.writeTo(this.ad, reg, value);
 };
+
 
 
 MAX30102.prototype.reset = function(){
@@ -52,34 +54,59 @@ MAX30102.prototype.reset = function(){
 
 MAX30102.prototype.init = function(){
   
-  this.write8(C.REG_INTR_ENABLE_1, 0xc0); // INTR setting
-  
+  this.write8(C.REG_INTR_ENABLE_1, 0xc0); // INTR setting  
   this.write8(C.REG_INTR_ENABLE_2, 0x00);
    
   this.write8(C.REG_FIFO_WR_PTR,0x00);  //FIFO_WR_PTR[4:0]
-   
   this.write8(C.REG_OVF_COUNTER, 0x00);  //OVF_COUNTER[4:0]
-    
   this.write8(C.REG_FIFO_RD_PTR, 0x00);  //FIFO_RD_PTR[4:0]
   
   this.write8(C.REG_FIFO_CONFIG, 0x0f);  //sample avg = 4, fifo rollover=false, fifo almost full = 17
-  
   this.write8(C.REG_MODE_CONFIG,0x03);  //0x02 for Red only, 0x03 for SpO2 mode 0x07 multimode LED
-  
   this.write8(C.REG_SPO2_CONFIG,0x27);  // SPO2_ADC range = 4096nA, SPO2 sample rate (100 Hz), LED pulseWidth (411uS)
     
   this.write8(C.REG_LED1_PA,0x24);  //Choose value for ~ 7mA for LED1
-
   this.write8(C.REG_LED2_PA,0x24);  // Choose value for ~ 7mA for LED2
-
   this.write8(C.REG_PILOT_PA,0x7f);   // Choose value for ~ 25mA for Pilot LED   
+  
 };
 
 
-MAX30102.prototype.read_fifo = function(){
+MAX30102.prototype.calibrate = function(register_data){
+
+  var un_temp;
+  var uch_temp;
+  
+  for(let i = 0; i < register_data.buffer_length; i++){
+    
+    this.read8(C.REG_INTR_STATUS_1);
+    this.read8(C.REG_INTR_STATUS_2);
+    this.write8(C.I2C_ADDR, C.REG_FIFO_DATA///////////////////////////////////////////////////continue here
+      
+    if(un_min>aun_red_buffer[i]){
+      un_min=aun_red_buffer[i];  //update signal min
+    };
+      
+    if(un_max<aun_red_buffer[i]){
+      un_max=aun_red_buffer[i];  //update signal max
+    };
+  };
+  
+};
+
+MAX30102.prototype.read_fifo_data = function(register_data){    ///fixme bitch
+  
+  while(digitalRead(interrupt_pin)){
+  
+  }
+  
   this.read8(C.REG_INTR_STATUS_1); 
   this.read8(C.REG_INTR_STATUS_2);
   return this.read8(C.I2C_READ_ADDR);
+};
+
+MAX30102.prototype.saturate_data = function(register_data, saturated_data){
+////////////////////
 };
 
 exports.connect = function(i2c) {
