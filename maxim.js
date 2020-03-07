f6fd6b493a088b5e57a83b31845cf99566ab5f10
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-07 03:26:14 -0600

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -119,6 +119,8 @@ MAX30102.prototype.getTemperature = function(saturated_data, unit){
   temp_data[0] = this.read8(C.REG_TEMP_INTR)[0];
   temp_data[1] = this.read8(C.REG_TEMP_FRAC)[0];
   
+  conosle.log(typeof(temp_data[0]);
+  
   temp = temp_data[0] + temp_data[1];
   
   if(unit == 0){
