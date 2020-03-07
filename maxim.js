9c6749a9fa722b6c261c0d963cd8edf76ee257b4
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-07 00:29:13 -0600

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -107,18 +107,19 @@ MAX30102.prototype.readTemperature = function(saturated_data, unit){
   
   this.write8(C.REG_TEMP_CONFIG, 0x01);
   
-  this.read8(C.REG_INTR_STATUS_1);
-  this.read8(C.REG_INTR_STATUS_2);
+  setTimeout( function() {
+    this.read8(C.REG_INTR_STATUS_1);
+    this.read8(C.REG_INTR_STATUS_2);
   
-  temp_data[0] = this.read8(C.REG_TEMP_INTR)[0];
-  temp_data[1] = this.read8(C.REG_TEMP_FRAC)[0];
+    temp_data[0] = this.read8(C.REG_TEMP_INTR)[0];
+    temp_data[1] = this.read8(C.REG_TEMP_FRAC)[0];
   
-  if(unit == 0){
-    saturated_data.temperature = temp_data[0] + temp_data[1];
-  }else{
-    saturated_data.temperature = 1.80 * (temp_data[0] + temp_data[1]) + 32.00;
-  }
-  
+    if(unit == 0){
+      saturated_data.temperature = temp_data[0] + temp_data[1];
+    }else{
+      saturated_data.temperature = 1.80 * (temp_data[0] + temp_data[1]) + 32.00;
+    }
+  }, 250 );
   
 };
 
