57ec23423adad93a5e7159e3a1268648fcd76920
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-07 01:39:06 -0600

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -103,23 +103,23 @@ MAX30102.prototype.read_fifo_data = function(register_data, i){    ///fixme bitc
 
 MAX30102.prototype.readTemperature = function(saturated_data, unit){
   
-  var temp_data = [0,0]   //[integer, fraction]
-  
   this.write8(C.REG_TEMP_CONFIG, 0x01);
   
-  setTimeout({ 
-    this.read8(C.REG_INTR_STATUS_1);
-    this.read8(C.REG_INTR_STATUS_2);
+  var temp_data = [0,0]   //[integer, fraction]
   
-    temp_data[0] = this.read8(C.REG_TEMP_INTR)[0];
-    temp_data[1] = this.read8(C.REG_TEMP_FRAC)[0];
+  while(pinRead(D31) == 1);
   
-    if(unit == 0){
-      saturated_data.temperature = temp_data[0] + temp_data[1];
-    }else{
-      saturated_data.temperature = 1.80 * (temp_data[0] + temp_data[1]) + 32.00;
-    }
-  }, 250 );
+  this.read8(C.REG_INTR_STATUS_1);
+  this.read8(C.REG_INTR_STATUS_2);
+  
+  temp_data[0] = this.read8(C.REG_TEMP_INTR)[0];
+  temp_data[1] = this.read8(C.REG_TEMP_FRAC)[0];
+  
+  if(unit == 0){
+    saturated_data.temperature = temp_data[0] + temp_data[1];
+  }else{
+    saturated_data.temperature = 1.80 * (temp_data[0] + temp_data[1]) + 32.00;
+  }
   
 };
 
