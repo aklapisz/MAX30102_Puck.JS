37bb56f461f62ac3b0ade07b8148ebf66d23a415
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-03-20 20:53:01 -0500

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -126,13 +126,13 @@ MAX30102.prototype.read_fifo_data = function(register_data,i){
   this.i2c.writeTo(this.ad, C.REG_FIFO_DATA);
   this.i2c.readFrom({this.ad, stop:false}, 1);
   
-  register_data.red_buffer[i] += this.i2c.readFrom({this.ad, stop:false}, 1)[0]<<16;
-  register_data.red_buffer[i] += this.i2c.readFrom({this.ad, stop:false}, 1)[0]<<8;
-  register_data.red_buffer[i] += this.i2c.readFrom({this.ad, stop:false}, 1)[0];
+  register_data.red_buffer[i] += (this.i2c.readFrom({this.ad, stop:false}, 1)[0])<<16;
+  register_data.red_buffer[i] += (this.i2c.readFrom({this.ad, stop:false}, 1)[0])<<8;
+  register_data.red_buffer[i] += (this.i2c.readFrom({this.ad, stop:false}, 1)[0]);
   
-  register_data.ir_buffer[i] += this.i2c.readFrom({this.ad, stop:false}, 1)[0]<<16;
-  register_data.ir_buffer[i] += this.i2c.readFrom({this.ad, stop:false}, 1)[0]<<8;
-  register_data.ir_buffer[i] += this.i2c.readFrom({this.ad, stop:true}, 1)[0];
+  register_data.ir_buffer[i] += (this.i2c.readFrom({this.ad, stop:false}, 1)[0])<<16;
+  register_data.ir_buffer[i] += (this.i2c.readFrom({this.ad, stop:false}, 1)[0])<<8;
+  register_data.ir_buffer[i] += (this.i2c.readFrom({this.ad, stop:true}, 1)[0]);
   
   register_data.red_buffer[i] &= 0x03FFFF;
   register_data.ir_buffer[i] &= 0x03FFFF;
