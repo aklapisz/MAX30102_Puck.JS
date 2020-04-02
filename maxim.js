40c75d7139f4cebc88f7ec07c09ea77ff335b3b8
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-04-01 19:14:19 -0500

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -131,8 +131,8 @@ MAX30102.prototype.init = function(){
   this.write8(C.REG_MODE_CONFIG,0x03);  //0x02 for Red only, 0x03 for SpO2 mode 0x07 multimode LED
   this.write8(C.REG_SPO2_CONFIG,0x27);  // SPO2_ADC range = 4096nA, SPO2 sample rate (100 Hz), LED pulseWidth (411uS)
     
-  this.write8(C.REG_LED1_PA,0x24);  //Choose value for ~ 7mA for LED1
-  this.write8(C.REG_LED2_PA,0x24);  // Choose value for ~ 7mA for LED2
+  this.write8(C.REG_LED1_PA,0x3f);  //Choose value for ~ 7mA for LED1
+  this.write8(C.REG_LED2_PA,0x3f);  // Choose value for ~ 7mA for LED2
   this.write8(C.REG_PILOT_PA,0x7f);   // Choose value for ~ 25mA for Pilot LED 
   
 };
