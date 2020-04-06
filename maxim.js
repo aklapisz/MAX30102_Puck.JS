0d504bbb6d89c68070ffeb9ba2a589640cb23c50
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-04-06 02:44:31 -0500

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -250,15 +250,17 @@ MAX30102.prototype.data_saturation = function(saturated_data){
   f_ir_mean = f_ir_mean/buffer_len;
   f_red_mean = f_red_mean/buffer_len;
   
+  console.log(f_ir_mean);
+  
 //remove DC from both buffers
   for(k=0; k<buffer_len; ++k){
     processingData.an_x[k] = register_data.ir_buffer[k] - f_ir_mean;
     processingData.an_y[k] = register_data.red_buffer[k] - f_red_mean;
   }
   
-  for(k=0;k<buffer_len;++k){
-    console.log(processingData.an_x[k]);
-  }
+  //for(k=0;k<buffer_len;++k){
+  //  console.log(processingData.an_x[k]);
+  //}
 
   
 //remove linear trend (baseline leveling)
