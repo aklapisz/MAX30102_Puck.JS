c9bc5a0c50e9daeb88b795483ad6fa8720129dac
aklapisz <35926117+aklapisz@users.noreply.github.com>
2020-04-06 01:56:53 -0500

    Update maxim.js

--- maxim.js
+++ maxim.js
@@ -242,9 +242,8 @@ MAX30102.prototype.data_saturation = function(saturated_data){
 
 
 //calculate DC mean of ir and red buffers
-
-  for(k=0; k<buffer_len; ++k){
-    console.log(register_data.ir_buffer[k]); 
+console.log(register_data.ir_buffer[k]);
+  for(k=0; k<buffer_len; ++k){ 
     f_ir_mean += register_data.ir_buffer[k];
     f_red_mean += register_data.red_buffer[k];
   }
